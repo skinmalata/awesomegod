0832e5e60d4abe88b914a6e6e93f146575c951bb
skinmalata <mesigotochukwu@gmail.com>
2026-08-19 16:05:07 +0100

    Update APK: w.png splash, logo icon, remove prayer request, hide status bar, bigger text, back button exits on home

--- android/app/src/main/java/com/awesomegodministries/app/MainActivity.java
+++ android/app/src/main/java/com/awesomegodministries/app/MainActivity.java
@@ -1,5 +1,32 @@
 package com.awesomegodministries.app;
 
+import android.os.Bundle;
+import android.view.View;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            );
+        }
+    }
+}
